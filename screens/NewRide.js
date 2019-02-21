85722120cdb0573c2c128f13b3c71494e2abfb23
Robbie Belson <rhbelson@gmail.com>
2019-02-21 11:49:12 -0600

    Added MapView

--- screens/NewRide.js
+++ screens/NewRide.js
@@ -1,21 +1,46 @@
 import React from 'react';
-import { ScrollView, StyleSheet, View, Text } from 'react-native';
+import { ScrollView, StyleSheet, View, Text, Button } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
 import { PickupButton } from "../components/pickup_button";
 import { DropoffButton } from "../components/drop-off_button";
 import { PickupDateButton } from "../components/pickup_date_button";
 import { PickupTimeButton } from "../components/pickup_time_button";
+import { Marker } from 'react-native-maps';
+import MapView from 'react-native-maps';
 
 export default class NewRide extends React.Component {
   static navigationOptions = {
     title: 'New Ride',
   };
+  constructor(props) {
+    // Required step: always call the parent class' constructor
+    super(props);
+
+    // Set the state directly. Use props if necessary.
+    this.state = {
+      region: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,}
+    }
+  }
+
+ 
+  onRegionChange(region) {
+    this.setState({ region });
+}
 
   render() {
     return (
       <View style={styles.container}>
         <PickupButton />
         <DropoffButton />
+        <MapView
+        style={{height:"50%"}}
+        region={this.state.region}
+        onRegionChange={() => this.onRegionChange()}
+      />
         <PickupDateButton />
         <PickupTimeButton />
       </View>
