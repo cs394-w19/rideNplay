ddad21d21253acf08dba38101b6b67d45bd76fe4
Ethan Zack <ethanzack2021@u.northwestern.edu>
2019-02-22 14:50:12 -0600

    avatars added

--- components/Ride.js
+++ components/Ride.js
@@ -8,21 +8,26 @@ import { Text,
          Dimensions
        } from 'react-native'
 import * as firebase from 'firebase'
+import { ListItem } from 'react-native-elements'
 
 this.width = Dimensions.get('window').width
 const Ride = (props) => {
     return(
-        <View style = {styles.info}>
-          <Text style = {styles.title}> {props.rideTitle} </Text>
-          <Text> {props.rideDesc} </Text>
-        </View>
+        <ListItem style = {styles.info}
+        leftAvatar={{
+          source: { uri: "http://images5.fanpop.com/image/photos/30200000/Nick-3-nick-miller-30219108-300-300.jpg" },
+          showEditButton: true,
+        }}
+        title = {props.rideTitle}
+        subtitle={props.rideDesc}
+        chevron>
+        </ListItem>
 
     )
 }
 
 const styles = StyleSheet.create({
   info: {
-    padding: 10,
     backgroundColor: '#edeeef',
     borderColor: '#858687',
     borderBottomWidth: 1,
