5d975d8e25f05d745457d76e1e8c1deabec38bf9
Yousef Issa <yousef@u.northwestern.edu>
2019-03-09 12:38:19 -0600

    added new ride endpoint

--- server.js
+++ server.js
@@ -20,68 +20,33 @@ express_app.get('*', function(req, res){
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-express_app.post('/api/messages', async (req, res) => {
-    console.log('Received new message request.');
+
+express_app.post('/api/new_ride', async (req, res) => {
+    // api to send texts after new ride creation.
+    console.log('Received new ride request.');
     res.header('Content-Type', 'application/json');
     const textData = req.body;
-    console.log(textData);
-    console.log(Date.now());
-    let messageCounter = 0;
-    // schedule texts for all timestamps following curve
-    req.body.ts.forEach(time => {
-        // add 10 seconds to account for request/scheduling task offset
-        scheduleText(textData, time + 10000);
-        messageCounter++;
-        console.log(scheduledTasks);
-    });
-    //
-    console.log(`${allTasks.length} tasks in total are scheduled`);
-    console.log({'success': true, 'messagesScheduled': messageCounter});
-    // return texts scheduled
-    return res.send({'success': true, 'messagesScheduled': messageCounter});
+    let newMessage = textData.message;
+    let messageRecipient = textData.to;
+    client.messages
+        .create({
+            from: "+17574186902",
+            to: messageRecipient,
+            body: newMessage
+        })
+        .then(() => {
+            // successful text send
+            console.log(`Message to ${body.to} successfully sent.`);
+            return res.send({'success': true, 'message': 'Successfully sent message.'});
+        })
+        .catch(err => {
+            // error sending text
+            console.log('hit error');
+            console.log(err);
+            return res.send({'success': false, 'message': err});
+        });
+
 });
-
-const scheduleText = async function(body, time) {
-
-    const messageTime = new Date(time);
-    const task_id = crypto.randomBytes(20).toString('hex');
-    scheduledTasks.add(task_id);
-
-    // run job on specific node cluster
-    if (cluster.isMaster) {
-        // schedule text message send
-        allTasks[task_id] = scheduler.scheduleJob(messageTime, function () {
-            if (scheduledTasks.has(task_id)) {
-                console.log('entered task');
-                // send text message with twilo service
-                let newMessage = body.message + '\n\nhttps://guarded-wildwood-37078.herokuapp.com/';
-                client.messages
-                    .create({
-                        from: "+17574186902",
-                        to: body.to,
-                        body: newMessage
-                    })
-                    .then(() => {
-                        console.log(`Message to ${body.to} successfully sent.`);
-                        console.log('Scheduled task canceled.');
-                        // cancel task so it doesn't double send (library bug)
-                        allTasks[task_id].cancel();
-                        // delete task from task objects
-                        delete allTasks[task_id]
-                    })
-                    .catch(err => {
-                        console.log('hit error');
-                        console.log(err);
-                    });
-            } else {
-                console.log('Message already sent');
-            }
-
-        }.bind(null, messageTime, task_id));
-        console.log(`Text scheduled at ${messageTime.toLocaleString()}.`);
-        return JSON.stringify({success: true});
-    }
-};
 
 // sever port is 3002
 express_app.portNumber = 3002;
